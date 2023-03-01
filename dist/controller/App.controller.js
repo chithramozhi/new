1cb83b0cd85ba3ef7833b468dd72ba64501cbc2b
chithramozhi <chithramozhisk15@gmail.com>
2023-03-01 16:23:21 +0530

    Add files via upload

---
+++ dist/controller/App.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/mvc/Controller","sap/ui/model/json/JSONModel"],function(e,o){"use strict";return e.extend("project1.controller.App",{onInit(){let e=new o("../model/data.json");this.getView().setModel(e,"portfolio")}})});
+//# sourceMappingURL=App.controller.js.map
